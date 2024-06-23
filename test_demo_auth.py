47c3b2f9669a9ff1a0fa58289296c4f8c8f03968
Elvira Mak <efilia@bk.ru>
2024-06-23 22:56:20 +0300

    first test demo auth

---
+++ test_demo_auth.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class FirstDemoAuth(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_first_demo_auth(self):
+        wd = self.wd
+        wd.get(
+            "https://idemo.bspb.ru/auth?response_type=code&client_id=1&redirect_uri=https%3A%2F%2Fidemo.bspb.ru%2Flogin%2Fsuccess&prefetch_uri=https%3A%2F%2Fidemo.bspb.ru%2Flogin%2Fprefetch&force_new_session=true")
+        wd.find_element_by_id("username").click()
+        wd.find_element_by_id("login-form").click()
+        wd.find_element_by_name("password").click()
+        wd.find_element_by_id("login-form").click()
+        wd.find_element_by_id("login-button").click()
+        wd.get("https://idemo.bspb.ru/auth/otp")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.wd.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.wd.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
